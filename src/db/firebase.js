1210e04f78b2d1bd642e2408a1d65335aeffc323
MRIGANKA-GUHA <mrigankaguha42@gmail.com>
2026-04-08 22:05:39 +0530

    Deploy changes

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,13 +1,15 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
 }
 
 export default admin;
